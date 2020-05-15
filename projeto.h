20fffe6f6a4b1700987fd51dad43077d382b305f
Mateuzz <38082919+vactus@users.noreply.github.com>
2020-05-15 18:26:17 -0300

    Update projeto.h

--- projeto.h
+++ projeto.h
@@ -23,19 +23,57 @@ typedef enum Status {ELIMINADO, PENDENTE, PASSOU, CAMPEAO} Status;
 typedef enum Estagio {NORMAL, PRORROGACAO, PENALTY} Estagio;
 typedef enum Fase {GRUPOS, OITAVAS, QUARTAS, SEMI, FINAL, PELO_TERCEIRO, FIM} Fase;
 
-typedef struct Horario {int dia, mes, horas, minutos;} Horario;
+typedef struct Horario {
+	int dia;
+	int mes;
+	int horas;
+	int minutos;
+} Horario;
 
-typedef struct Info {int jogos_realizados, times_cadastrados, times_classificados; Horario ultima_data; int jogos_pareados;} Info;
+typedef struct Info {
+	int jogos_realizados;
+	int times_cadastrados;
+	int times_classificados;
+	Horario ultima_data;
+	int jogos_pareados;
+} Info;
 
-typedef struct Time {Status status; char nome[MAX_NAME_LEN + 1]; Id id; int vitorias, derrotas, gols, gols_sofridos, pontos, empates;} Time;
+typedef struct Time {
+	Status status;
+	char nome[MAX_NAME_LEN + 1];
+	Id id;
+	int vitorias;
+	int derrotas;
+	int gols;
+	int gols_sofridos;
+	int pontos;
+	int empates;
+} Time;
 
-typedef struct Placar {int vencedor, perdedor;} Placar;
+typedef struct Placar {
+	int vencedor;
+	Id perdedor;
+} Placar;
 
-typedef struct Resultado {Placar placar; Estagio estagio;Id vencedor, perdedor; bool empate;} Resultado;
+typedef struct Resultado {
+	Placar placar;
+	Estagio estagio;
+	Id vencedor;
+	Id perdedor;
+	bool empate;
+} Resultado;
 
-typedef struct Jogo {Horario data; Fase fase; Resultado resultado;
-                      Id id; char local[MAX_NAME_LEN + 1];} Jogo;
+typedef struct Jogo {
+	Horario data;
+	Fase fase;
+	Resultado resultado;
+        Id id;
+	char local[MAX_NAME_LEN + 1];
+} Jogo;
 
-typedef struct Chave {Id t1; Id t2;} Chave;
+typedef struct Chave {
+	Id t1;
+	Id t2;
+} Chave;
 
 #endif
