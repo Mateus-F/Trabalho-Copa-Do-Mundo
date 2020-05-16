769d33974822541ddb3a3664d1cb9fc532f35681
Mateuzz <38082919+vactus@users.noreply.github.com>
2020-05-16 16:52:39 -0300

    Update files.h

--- files.h
+++ files.h
@@ -1,10 +1,14 @@
 #ifndef FILES_H
 #define FILES_H
 
+#include <stdbool.h>
+
 #include "projeto.h"
 
 int abrir_lista(char lista[][MAX_NAME_LEN + 1], size_t size, char *filename);
 int meu_atoi(const char *str);
 char *fgets_(char *str, size_t size, FILE *io_stream);
+void toupper_all(char *str);
+bool preposicao(char *str);
 
 #endif
