f59de106d5ba00ac04a94c65a46ecd33b6140760
Mateuzz <38082919+Mateus-F@users.noreply.github.com>
2020-05-20 19:49:11 -0300

    Update files.c

--- files.c
+++ files.c
@@ -5,89 +5,87 @@
 #include "files.h"
 #include "projeto.h"
 
-int
-abrir_lista(char lista[][MAX_NAME_LEN + 1], size_t size, char *filename)
+static bool preposicao(char *str);
+
+int abrir_lista(char lista[][MAX_NAME_LEN + 1], size_t size, char *filename)
 {
-	FILE *file = fopen(filename, "r");
-	
-	if (file == NULL) {
-    		fprintf(stderr, "O arquivo não pôde ser aberto\n");
-	        return 0;
-	}
-	
-	int i = 0;
-	char buff[MAX_NAME_LEN + 1];
+    FILE *file = fopen(filename, "r");
 
-    	while (i < size) {
-    		if (fgets_(buff, MAX_NAME_LEN, file) == NULL)
-    			break;
-        	strcpy(lista[i], buff);
-        	++i;
-    	}
-    	fclose(file);
+    if (file == NULL) {
+        fprintf(stderr, "O arquivo não pôde ser aberto\n");
+        return 0;
+    }
 
-    	return i;
+    int i = 0;
+    char buff[MAX_NAME_LEN + 1];
+
+    while (i < size) {
+        if (fgets_(buff, MAX_NAME_LEN, file) == NULL)
+            break;
+        strcpy(lista[i], buff);
+        ++i;
+    }
+    fclose(file);
+
+    return i;
 }
 
-char *
-fgets_(char *str, size_t size, FILE *stream)
+char *fgets_(char *str, size_t size, FILE *stream)
 {
-	int ch;
-	unsigned i = 0;
+    int ch;
+    unsigned i = 0;
 
-	while ((ch = getc(stream)) != '\n' && ch != EOF)
-		if (i < size)
-			str[i++] = ch;
-	if (ch == EOF && i == 0)
-		return NULL;
-	str[i] = '\0';
-	return str;
+    while ((ch = getc(stream)) != '\n' && ch != EOF)
+        if (i < size)
+            str[i++] = ch;
+    if (ch == EOF && i == 0)
+        return NULL;
+    str[i] = '\0';
+    return str;
 }
 
-int
-meu_atoi(const char *str)
+int meu_atoi(const char *str)
 {
-	while (isspace(*str))
-		str++;
-	if (!isdigit(*str))
-		return EOF;
-	int numero = 0;
-	while (isdigit(*str)) {
-		numero *= 10;
-		numero += *str - '0';
-		++str;
-	}
-	return numero;
+    while (isspace(*str))
+        str++;
+    if (!isdigit(*str))
+        return EOF;
+    int numero = 0;
+    while (isdigit(*str)) {
+        numero *= 10;
+        numero += *str - '0';
+        ++str;
+    }
+    return numero;
 }
 
-void 
-toupper_all(char *str)
+void toupper_all(char *str)
 {
-	bool out = true;
+    bool out = true;
 
-	while (*str != '\0') {
-		if (isalpha(*str)) {
-			if (out && !preposicao(str))
-				*str = toupper(*str);
-			out = false;
-		} else {
-			out = true;
-		}
-		++str;
-	}
+    while (*str != '\0') {
+        if (isalpha(*str)) {
+            if (out && !preposicao(str))
+                *str = toupper(*str);
+            out = false;
+        } else {
+            out = true;
+        }
+        ++str;
+    }
 }
 
-bool 
+static bool 
 preposicao(char *str)
 {
-	int count = 0;
+    int count = 0;
 
-	while (isalpha(*str)) {
-		if (count == 0 && *str != 'd')
-			return false;
-		++count;
-		++str;
-	}
-	
-	return count == 2;
+    while (isalpha(*str)) {
+        if (count == 0 && *str != 'd')
+            return false;
+        ++count;
+        ++str;
+    }
+
+    return count == 2;
 }
