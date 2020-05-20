26ced84e60ab17f5b3eb0cc235f74ec32d0d9023
Mateuzz <38082919+Mateus-F@users.noreply.github.com>
2020-05-20 19:48:43 -0300

    Update files.h

--- files.h
+++ files.h
@@ -9,6 +9,5 @@ int abrir_lista(char lista[][MAX_NAME_LEN + 1], size_t size, char *filename);
 int meu_atoi(const char *str);
 char *fgets_(char *str, size_t size, FILE *io_stream);
 void toupper_all(char *str);
-bool preposicao(char *str);
 
 #endif
